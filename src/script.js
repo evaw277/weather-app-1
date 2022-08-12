186b06a360be19000fbcd899c8263f7706da7cf7
evaw277 <evaweisneck@gmail.com>
2022-08-11 20:28:56 -0400

    removed unit conversion

--- src/script.js
+++ src/script.js
@@ -44,7 +44,7 @@ h1.innerHTML = `${hours}:${minutes}, ${day}  ${month} ${date}, ${year}`;
 
 function getForecast(coordinates) {
   let apiKey = "a5dcf72a868ff3572b4a1a8358099667";
-  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
+  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=imperial`;
   axios.get(apiUrl).then(displayForecast);
 }
 
@@ -54,7 +54,7 @@ function showWeather(response) {
   currentTemperature.innerHTML = `${temperature}`;
   let windSpeed = document.querySelector("#wind");
   let humidity = document.querySelector("#humidity");
-  windSpeed.innerHTML = `wind: ${Math.round(response.data.wind.speed)} km/h`;
+  windSpeed.innerHTML = `wind: ${Math.round(response.data.wind.speed)} mph`;
   humidity.innerHTML = `humidity: ${response.data.main.humidity}%`;
 
   let city = document.querySelector("h2");
@@ -82,9 +82,8 @@ function search(event) {
 
   if (city.value) {
     h2.innerHTML = `{$city.value}`;
-    let units = "metric";
     let apiKey = "a5dcf72a868ff3572b4a1a8358099667";
-    let url = `https://api.openweathermap.org/data/2.5/weather?q=${city.value}&appid=${apiKey}&units=${units}`;
+    let url = `https://api.openweathermap.org/data/2.5/weather?q=${city.value}&appid=${apiKey}&units=imperial`;
     axios.get(url).then(showWeather);
   } else {
     alert("Please type a city");
@@ -138,29 +137,12 @@ function currentLocation(position) {
   let apiKey = "a5dcf72a868ff3572b4a1a8358099667";
   let lat = position.coords.latitude;
   let lon = position.coords.longitude;
-  let url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&units=metric&appid=${apiKey}`;
+  let url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&units=imperial&appid=${apiKey}`;
   axios.get(url).then(showWeather);
 }
 function getLocation(event) {
   event.preventDefault();
   navigator.geolocation.getCurrentPosition(currentLocation);
-}
-
-function displayFahrenheitTemperature(event) {
-  event.preventDefault();
-  celsiusLink.classList.remove("active");
-  farenheitLink.classList.add("active");
-  let temperatureElement = document.querySelector("#temperature");
-  let fahrenheitTemperature = (celsiusTemperature * 9) / 5 + 32;
-  temperatureElement.innerHTML = Math.round(fahrenheitTemperature);
-}
-
-function displayCelsiusTemperature(event) {
-  event.preventDefault();
-  celsiusLink.classList.add("active");
-  farenheitLink.classList.remove("active");
-  let temperatureElement = document.querySelector("#temperature");
-  temperatureElement.innerHTML = Math.round(celsiusTemperature);
 }
 
 let form = document.querySelector("#search-box");
@@ -169,16 +151,7 @@ form.addEventListener("submit", search);
 let button = document.querySelector("#current-button");
 button.addEventListener("click", getLocation);
 
-let farenheitLink = document.querySelector("#f-units");
-farenheitLink.addEventListener("click", displayFahrenheitTemperature);
-
-let celsiusLink = document.querySelector("#c-units");
-celsiusLink.addEventListener("click", displayCelsiusTemperature);
-
-let celsiusTemperature = null;
-
 let apiKey = "a5dcf72a868ff3572b4a1a8358099667";
 let city = "New York";
-let units = "metric";
-let url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=${units}`;
+let url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=imperial`;
 axios.get(url).then(showWeather);
